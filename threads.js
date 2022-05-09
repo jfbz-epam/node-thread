e66e4cd2a548cb46b1a4f16ba10a6c467336a629
jfbz-epam <juan_bustamante@epam.com>
2022-05-09 11:01:36 -0500

    initial commit

---
+++ threads.js
@@ -0,0 +1,14 @@
+process.env.UV_THREADPOOL_SIZE =6;
+const crypto = require("crypto");
+const start = Date.now();
+
+function logHashTime() {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    console.log("Hash: ", Date.now() - start);
+  });
+}
+logHashTime();
+logHashTime();
+logHashTime();
+logHashTime();
+logHashTime();
